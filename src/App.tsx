194c7df08ccde652605a8404a27b4a7ca3e3dc29
Bamidele Isaac <bamideleisaac11@gmail.com>
2022-06-22 10:21:34 +0100

    feat: test1

--- src/App.tsx
+++ src/App.tsx
@@ -5,64 +5,51 @@ import {
   getWeek,
   subDays,
   subWeeks,
-} from "date-fns";
-import { addDays } from "date-fns/esm";
-import React, { useState } from "react";
-import "./App.css";
+  addDays,
+} from 'date-fns';
+import React, { useState } from 'react';
+import './App.css';
 
-const DayComp = ({
-  dayArr,
-  todaysDate,
-}: {
-  dayArr: Date[];
-  todaysDate: Date;
-}) => {
-  const initialSelectedDay = format(todaysDate, "d");
+function DayComp({ dayArr, todaysDate }: { dayArr: Date[]; todaysDate: Date }) {
+  const initialSelectedDay = format(todaysDate, 'd');
   const [selectedDay, setSelectedDay] = useState(initialSelectedDay);
   const days = dayArr.map((dayObj) => {
-    const today =
-      dayObj.toDateString() === todaysDate.toDateString()
-        ? "calendar__today"
-        : "";
-    const [day, dayNumber] = format(dayObj, "E d").split(" ");
+    const today = dayObj.toDateString() === todaysDate.toDateString()
+      ? 'calendar__today'
+      : '';
+    const [day, dayNumber] = format(dayObj, 'E d').split(' ');
     return (
-      <div
-        className={`${
-          dayNumber === selectedDay
-            ? "calendar__active calendar__day"
-            : "calendar__day"
-        } ${today}`}
+      <button
+        type="button"
+        className={` ${dayNumber === selectedDay
+          ? 'calendar__active calendar__day'
+          : 'calendar__day'} ${today} animated-button`}
         onClick={() => {
           setSelectedDay(dayNumber);
         }}
       >
-        <div>{day}</div>
-        <div>{dayNumber}</div>
-      </div>
+        <span>{day}</span>
+        <span>{dayNumber}</span>
+      </button>
     );
   });
-  return <div className={"calendar__days"}>{days}</div>;
-};
+  return <div className="calendar__days">{days}</div>;
+}
 
-const GetRange = ({ date, todaysDate }: { date: Date; todaysDate: Date }) => {
+function GetRange({ date, todaysDate }: { date: Date; todaysDate: Date }) {
   const threeDaysAfter = addDays(date, 3);
   const threeDaysBehind = subDays(date, 3);
-
   const dayArr = eachDayOfInterval({
     start: threeDaysBehind,
     end: threeDaysAfter,
   });
-
   return <DayComp todaysDate={todaysDate} dayArr={dayArr} />;
-};
+}
 
-const App = () => {
+function App() {
   const date = new Date();
-
   const [currentWk, setCurrentWk] = useState(getWeek(date));
-
   const [currentDate, setCurrentDate] = useState(date);
-
   const handleNext = () => {
     setCurrentWk(currentWk + 1);
     setCurrentDate(addWeeks(currentDate, 1));
@@ -75,20 +62,26 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{"Ergeon Test"}</h1>
+        <h1>Ergeon Test</h1>
       </header>
       <div className="calendar">
         <div className="calendar_head">
-          <button onClick={handlePrev}>{"<<<"}</button>
+          <button type="button" className="slide slide-left" onClick={handlePrev}>
+            <i className="icon-arrow-left" />
+            <span>Prev</span>
+          </button>
           <p>{`week ${currentWk}`}</p>
-          <button onClick={handleNext}>{">>>"}</button>
+          <button type="button" className="slide slide-right" onClick={handleNext}>
+            <span>Next</span>
+            <i className="icon-arrow-right" />
+          </button>
         </div>
         <div className="calendar_body">
-          {<GetRange date={currentDate} todaysDate={date} />}
+          <GetRange date={currentDate} todaysDate={date} />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
